0368752374fff68e9b1fbd2182b1d64660d47ce5
Eugene Song <yougene42193@gmail.com>
2019-03-26 21:35:18 -0400

    changed DATABASEURL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://Eugene@localhost/noteful',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://Eugene@localhost:5432/noteful',
   }
